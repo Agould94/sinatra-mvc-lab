2ce2e5abd4429029e8fc4a26b35ffc81a7114352
Alexander Gould <alexander.gould94@gmail.com>
2021-05-03 13:06:18 -0700

    commit tree 0af3df1f326a8f107b681043b7edd2a66e31d7d6

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+    set :views, settings.root + '/4:26-4:30/sinatra-mvc-lab/views'
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        pl = PigLatinizer.new
+        @piglatin = pl.piglatinize(params[:user_phrase])
+        erb :results
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class PigLatinizer
+
+    def piglatinize(text)
+        t = text.split(' ')
+        b = t.map {|word| piglatinize_word(word)}
+        b.join(" ")
+    end
+
+    def vowel?(char)
+        char.match(/[aAeEiIoOuU]/)
+    end
+
+    def piglatinize_word(word)
+        if vowel?(word[0])
+            word + "way"
+        else
+            vowel_index = word.index(/[aAeEiIoOuU]/)
+            consonants = word.slice(0..vowel_index-1)
+            word_leftover = word.slice(vowel_index..word.length)
+            word_leftover+consonants+"ay"
+        end
+    end
+end
+#binding.pry
